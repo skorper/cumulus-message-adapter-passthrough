b8387de08d5f47a5b0a7c7d2b6aa55af3c1e6eae
Yen, David (398B-Affiliate) <david.yen@jpl.nasa.gov>
2022-03-23 10:32:53 -0700

    new build script

--- build.sh
+++ build.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
-service_name=`poetry version | awk '{print $1}'`
-code_version=`poetry version | awk '{print $2}'`
-pwd;mkdir venv;mkdir -p build/lambda;mkdir -p build/dist
+rm -Rf venv build;mkdir venv;mkdir -p build
 poetry config virtualenvs.path venv
 poetry install --no-dev
 cp ./*.py venv/*/lib/*/site-packages/
 cp -R venv/*/lib/*/site-packages/* build
 
 cd build;zip -r ../artifact.zip .
-rm -Rf build
+
