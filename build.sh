36d88b97c08c40c23b67b7370b5171de9347aa07
Yen, David (398B-Affiliate) <david.yen@jpl.nasa.gov>
2022-03-23 10:57:21 -0700

    use cp -Ra for ubuntu comaptibility

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@ rm -Rf venv build;mkdir venv;mkdir -p build
 poetry config virtualenvs.path venv
 poetry install --no-dev
 cp ./*.py venv/*/lib/*/site-packages/
-cp -R venv/*/lib/*/site-packages/* build
+ls -la venv/*/lib/*/site-packages/
+cp -Ra venv/*/lib/*/site-packages/* build
 
 cd build;zip -r ../artifact.zip .
 
